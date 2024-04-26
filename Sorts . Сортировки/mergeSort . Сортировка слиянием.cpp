37fa7d4bc21473c460c4be8493e76ec27409f35c
SSK <anehkasi24@gmail.com>
2024-04-26 21:38:05 +0300

    Создана сортировка слиянием

---
+++ Sorts . Сортировки/mergeSort . Сортировка слиянием.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> mergeSortHelper(vector<int>& V, int start, int end)
+{
+    if (start == end)
+    {
+        return {V[start]};    
+    }
+    else
+    {
+        vector<int> v1 = mergeSortHelper(V, start, start + (end - start) / 2), v2 = mergeSortHelper(V, start + (end - start) / 2 + 1, end);
+        vector<int> rez(v1.size() + v2.size());
+        int i = 0, j = 0;
+        while (i < v1.size() && j < v2.size())
+        {
+            if (v1[i] < v2[j])
+            {
+                rez[i + j] = v1[i];
+                i++;
+            }
+            else
+            {
+                rez[i + j] = v2[j];
+                j++;
+            }
+        }
+        while (i < v1.size())
+        {
+            rez[i + j] = v1[i];
+            i++;
+        }
+        while (j < v2.size())
+        {
+            rez[i + j] = v2[j];
+            j++;
+        }
+        return rez;
+    }
+}
+
+vector<int> mergeSort(vector<int> V)
+{
+    return mergeSortHelper(V, 0, V.size() - 1);
+}
+
+int main()
+{
+	vector<int> V = {1, 2, 3, 9, 7, 4, 4, 2};
+	for (int i = 0; i < V.size(); i++)
+		cout << V[i] << ' ';
+	cout << endl;
+	V = mergeSort(V);
+	for (int i = 0; i < V.size(); i++)
+		cout << V[i] << ' ';
+}
