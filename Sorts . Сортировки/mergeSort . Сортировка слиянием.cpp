fa113bda4c99a55376806fd7b99d94a9ea125043
SSKmath <144555420+SSKmath@users.noreply.github.com>
2024-10-27 19:08:34 +0300

    Update mergeSort . Сортировка слиянием.cpp

--- Sorts . Сортировки/mergeSort . Сортировка слиянием.cpp
+++ Sorts . Сортировки/mergeSort . Сортировка слиянием.cpp
@@ -3,45 +3,46 @@
 
 using namespace std;
 
-vector<int> mergeSortHelper(vector<int>& V, int start, int end)
+vector<int> mergeSortHelper(const vector<int>& V, int start, int end)
 {
     if (start == end)
-    {
         return {V[start]};    
-    }
-    else
+    
+    int mid = start + (end - start) / 2;
+    vector<int> v1 = mergeSortHelper(V, start, mid), 
+    vector<int> v2 = mergeSortHelper(V, mid + 1, end);
+
+    vector<int> rez(v1.size() + v2.size());
+    
+    int i = 0, j = 0;
+    while (i < v1.size() && j < v2.size())
     {
-        vector<int> v1 = mergeSortHelper(V, start, start + (end - start) / 2), v2 = mergeSortHelper(V, start + (end - start) / 2 + 1, end);
-        vector<int> rez(v1.size() + v2.size());
-        int i = 0, j = 0;
-        while (i < v1.size() && j < v2.size())
-        {
-            if (v1[i] < v2[j])
-            {
-                rez[i + j] = v1[i];
-                i++;
-            }
-            else
-            {
-                rez[i + j] = v2[j];
-                j++;
-            }
-        }
-        while (i < v1.size())
+        if (v1[i] < v2[j])
         {
             rez[i + j] = v1[i];
             i++;
         }
-        while (j < v2.size())
+        else
         {
             rez[i + j] = v2[j];
             j++;
         }
-        return rez;
     }
+    while (i < v1.size())
+    {
+        rez[i + j] = v1[i];
+        i++;
+    }
+    while (j < v2.size())
+    {
+        rez[i + j] = v2[j];
+        j++;
+    }
+    
+    return rez;
 }
 
-vector<int> mergeSort(vector<int> V)
+vector<int> mergeSort(const vector<int>& V)
 {
     return mergeSortHelper(V, 0, V.size() - 1);
 }
@@ -49,9 +50,11 @@ vector<int> mergeSort(vector<int> V)
 int main()
 {
 	vector<int> V = {1, 2, 3, 9, 7, 4, 4, 2};
+
 	for (int i = 0; i < V.size(); i++)
 		cout << V[i] << ' ';
 	cout << endl;
+
 	V = mergeSort(V);
 	for (int i = 0; i < V.size(); i++)
 		cout << V[i] << ' ';
